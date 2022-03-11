fdb6d7672f20fb54a999fde1646fea57023b5157
Globoplox <globoplox@gmail.com>
2022-03-11 23:27:55 +0100

    Add hadf assed literal global initialization

--- src/stacklang/compiler/unit.cr
+++ src/stacklang/compiler/unit.cr
@@ -14,16 +14,16 @@ class Stacklang::Unit
     getter symbol
     getter type_info
     getter initialization
+    @initialization : Stacklang::AST::Expression?
     
     # for now globals are zero initialized
-    def initialize(@name : String, @type_info : Type::Any, @initialization : Int32 = 0) #initialisation Int32 is a placeholder
+    def initialize(@name : String, @type_info : Type::Any, @initialization)
       @symbol = "__global_#{name}"
     end
 
     def initialize(@symbol : String)
       @name = @symbol
       @type_info = Type::Word.new
-      @initialization = 0
     end
   end
   
@@ -110,8 +110,8 @@ class Stacklang::Unit
 
   def self_globals : Array(Global)
     @self_globals ||= @ast.globals.map do |variable|
-      raise "Initialization of global variable is not implemented" if variable.initialization
-      Global.new variable.name.name, Type::Any.solve_constraint variable.constraint, structs
+      #raise "Initialization of global variable is not implemented" if variable.initialization
+      Global.new variable.name.name, Type::Any.solve_constraint(variable.constraint, structs), initialization: variable.initialization
     end
   end
 
@@ -134,12 +134,30 @@ class Stacklang::Unit
   def compile
     RiSC16::Object.new(path.to_s).tap do |object|
       object.sections << RiSC16::Object::Section.new("globals").tap do |section|
-        size = self_globals.reduce(0u16) do |size, local|
+        code = [] of RiSC16::Word
+        self_globals.each do |local|
           raise "Duplicate local global #{local.name} in #{path}" if section.definitions[local.symbol]?
-          section.definitions[local.symbol] = RiSC16::Object::Section::Symbol.new size.to_i32, true
-          size + local.type_info.size
+          section.definitions[local.symbol] = RiSC16::Object::Section::Symbol.new code.size, true
+          if local.initialization
+          # we can do _ = word, *_ = &identifier
+            case local.type_info
+            when Type::Word # There is code duplication here with function var init/typechecking
+              local.initialization.as?(Stacklang::AST::Literal).try &.tap do |literal|
+                code << literal.number.to_u16! # TODO: better error message, resuse function logic ?
+              end || raise "Type does not match affected value initializing global #{local.name} of type #{local.type_info} with value #{local.initialization}"
+            # when Type::Pointer # do later, allows to initialize with dereferenced identifier. Typecheck. Would work only on var since
+            # there are no func ptr type yet
+            #when Type::Table # do later, need recursive stuff, annoying
+            #when Type::Struct # do later, need recursive stuff, annoying
+            else "Intializing global #{local.name} of type #{local.type_info} is not supported yet"
+            end
+          else
+            local.type_info.size.times do
+              code << 0u16
+            end
+          end
         end
-        section.text = Slice(UInt16).new size
+        section.text = Slice.new code.size do |i| code[i] end # TODO ugly, fix
       end
       self_functions.each do |function| 
         object.sections << function.compile
